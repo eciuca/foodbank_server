c01301cde3b19ce810065184d59b2a2ebfa6d9c0
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-04-03 07:54:27 +0300

    Extract variables

--- foodbank-keycloak-user-provider/src/main/java/foodbank/it/keycloak/DbUtil.java
+++ foodbank-keycloak-user-provider/src/main/java/foodbank/it/keycloak/DbUtil.java
@@ -6,10 +6,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import static foodbank.it.keycloak.CustomUserStorageProviderConstants.*;
+
 public class DbUtil {
 
 	public static Connection getConnection(ComponentModel config) throws SQLException {
 		String driverClass = config.get(CustomUserStorageProviderConstants.CONFIG_KEY_JDBC_DRIVER);
+
 		try {
 			Class.forName(driverClass);
 		}
@@ -17,9 +20,10 @@ public class DbUtil {
 			// ... error handling omitted
 		}
 
-		return DriverManager.getConnection(
-				config.get(CustomUserStorageProviderConstants.CONFIG_KEY_JDBC_URL),
-				config.get(CustomUserStorageProviderConstants.CONFIG_KEY_DB_USERNAME),
-				config.get(CustomUserStorageProviderConstants.CONFIG_KEY_DB_PASSWORD));
+		String url = config.get(CONFIG_KEY_JDBC_URL);
+		String user = config.get(CONFIG_KEY_DB_USERNAME);
+		String password = config.get(CONFIG_KEY_DB_PASSWORD);
+
+		return DriverManager.getConnection(url, user, password);
 	}
 }
