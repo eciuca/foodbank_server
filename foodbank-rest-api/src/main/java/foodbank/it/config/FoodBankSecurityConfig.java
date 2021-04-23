2bda9e6f0fb0ac886d3f1c2cdac1b1bd37e53768
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-04-23 07:44:38 +0300

    Disable security and auto ddl temporarily

--- foodbank-rest-api/src/main/java/foodbank/it/config/FoodBankSecurityConfig.java
+++ foodbank-rest-api/src/main/java/foodbank/it/config/FoodBankSecurityConfig.java
@@ -11,7 +11,7 @@ public class FoodBankSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().disable()
 				.authorizeRequests()
-				.anyRequest().authenticated()
+				.anyRequest().permitAll()
 				.and()
 				.oauth2ResourceServer()
 				.jwt();
