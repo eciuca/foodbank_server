8a12fec1389de7d279b208506df7883af43bfd3a
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2021-04-30 14:41:31 +0300

    Update realm and Keycloak SPI provider

--- foodbank-keycloak-user-provider/src/main/java/foodbank/it/keycloak/FoodbankMysqlUser.java
+++ foodbank-keycloak-user-provider/src/main/java/foodbank/it/keycloak/FoodbankMysqlUser.java
@@ -2,26 +2,38 @@ package foodbank.it.keycloak;
 
 import org.keycloak.common.util.MultivaluedHashMap;
 import org.keycloak.component.ComponentModel;
+import org.keycloak.models.GroupModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
+import org.keycloak.models.RoleModel;
 import org.keycloak.storage.adapter.AbstractUserAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-public class CustomUser extends AbstractUserAdapter {
+public class FoodbankMysqlUser extends AbstractUserAdapter {
+
+	private final Logger LOG = LoggerFactory.getLogger(FoodbankMysqlUser.class);
 
 	private final String username;
 	private final String firstName;
 	private final String lastName;
 	private final String email;
+	private final String groups;
 
-	public CustomUser(KeycloakSession session, RealmModel realm, ComponentModel storageProviderModel, String username, String firstName, String lastName, String email) {
+	public FoodbankMysqlUser(KeycloakSession session, RealmModel realm, ComponentModel storageProviderModel, String username, String firstName, String lastName, String email, String groups) {
 		super(session, realm, storageProviderModel);
 		this.username = username;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
+		this.groups = groups;
 	}
 
 	public static CustomUserBuilder customUser(KeycloakSession keycloakSession, RealmModel realmModel, ComponentModel componentModel, String username) {
@@ -30,12 +42,40 @@ public class CustomUser extends AbstractUserAdapter {
 
 	@Override
 	public Map<String, List<String>> getAttributes() {
+		LOG.info("Getting user attributes...");
 		MultivaluedHashMap<String, String> attributes = new MultivaluedHashMap<>();
 		attributes.add(USERNAME, getUsername());
 		attributes.add(EMAIL, getEmail());
 		attributes.add(FIRST_NAME, getFirstName());
 		attributes.add(LAST_NAME, getLastName());
+		LOG.info(attributes.toString());
 		return attributes;
+	}
+
+	@Override
+	public boolean isMemberOf(GroupModel group) {
+		return super.isMemberOf(group);
+	}
+
+	@Override
+	public Set<GroupModel> getGroupsInternal() {
+		LOG.info("Groups is: " + groups);
+		List<String> groupNames = Arrays.asList(groups.toLowerCase().split(" "));
+		Set<GroupModel> groupModelSet = realm.getGroupsStream()
+				.filter(groupModel -> groupNames.contains(groupModel.getName().toLowerCase()))
+				.collect(Collectors.toSet());
+		LOG.info("Result: " + groupModelSet.stream().map(GroupModel::getName).collect(Collectors.joining(", ")));
+
+		return groupModelSet;
+	}
+
+	@Override protected Set<RoleModel> getRoleMappingsInternal() {
+		return super.getRoleMappingsInternal();
+	}
+
+	@Override
+	public boolean hasRole(RoleModel role) {
+		return super.hasRole(role);
 	}
 
 	public static class CustomUserBuilder {
@@ -46,6 +86,7 @@ public class CustomUser extends AbstractUserAdapter {
 		private String firstName;
 		private String lastName;
 		private String email;
+		private String groups;
 
 		public CustomUserBuilder(KeycloakSession keycloakSession, RealmModel realmModel, ComponentModel componentModel, String username) {
 			this.keycloakSession = keycloakSession;
@@ -54,8 +95,8 @@ public class CustomUser extends AbstractUserAdapter {
 			this.username = username;
 		}
 
-		public CustomUser build() {
-			return new CustomUser(keycloakSession, realmModel, componentModel, username, firstName, lastName, email);
+		public FoodbankMysqlUser build() {
+			return new FoodbankMysqlUser(keycloakSession, realmModel, componentModel, username, firstName, lastName, email, groups);
 		}
 
 		public CustomUserBuilder withFirstName(String firstName) {
@@ -70,6 +111,11 @@ public class CustomUser extends AbstractUserAdapter {
 
 		public CustomUserBuilder withEmail(String email) {
 			this.email = email;
+			return this;
+		}
+
+		public CustomUserBuilder withGroups(String groups) {
+			this.groups = groups;
 			return this;
 		}
 	}
--- foodbank-keycloak-user-provider/src/main/java/foodbank/it/keycloak/KeycloakFoodBankUserProvider.java
+++ foodbank-keycloak-user-provider/src/main/java/foodbank/it/keycloak/KeycloakFoodBankUserProvider.java
@@ -4,15 +4,15 @@ import at.favre.lib.crypto.bcrypt.BCrypt;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.credential.CredentialInput;
 import org.keycloak.credential.CredentialInputValidator;
-import org.keycloak.models.GroupModel;
-import org.keycloak.models.KeycloakSession;
-import org.keycloak.models.RealmModel;
-import org.keycloak.models.UserModel;
+import org.keycloak.models.*;
 import org.keycloak.models.credential.PasswordCredentialModel;
 import org.keycloak.storage.StorageId;
 import org.keycloak.storage.UserStorageProvider;
 import org.keycloak.storage.user.UserLookupProvider;
 import org.keycloak.storage.user.UserQueryProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import sun.rmi.runtime.Log;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -24,12 +24,16 @@ import java.util.Map;
 
 public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLookupProvider, CredentialInputValidator, UserQueryProvider {
 
+	private static final Logger LOG = LoggerFactory.getLogger(KeycloakFoodBankUserProvider.class);
+
 	public static final String OFFSET_LIMIT = " OFFSET ? LIMIT ?";
 	public static final String USER_COUNT = "select count(*) from t_user";
 	public static final String PASSWORD_BY_USERNAME = "select password from t_user where id_user = ?";
-	public static final String USERS = "select id_user, user_name, email from t_user";
+	public static final String USERS = "select id_user, user_name, email, rights from t_user";
 	public static final String USERS_LOOKUP = USERS + " where id_user like ? or user_name like ? or email like ?";
 	public static final String USERS_LOOKUP_PAGE = USERS_LOOKUP + OFFSET_LIMIT;
+	public static final String USERS_BY_RIGHTS = USERS + " where rights like ?";
+	public static final String USERS_BY_RIGHTS_PAGE = USERS_BY_RIGHTS + OFFSET_LIMIT;
 	public static final String USER_BY_ID = USERS + " where id_user = ?";
 	public static final String USER_BY_USERNAME = USERS + " where id_user = ?";
 	public static final String USER_BY_EMAIL = USERS + " where email = ?";
@@ -58,6 +62,7 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 
 	@Override
 	public boolean isValid(RealmModel realmModel, UserModel userModel, CredentialInput credentialInput) {
+		LOG.info("Validating user ...");
 		if (!this.supportsCredentialType(credentialInput.getType())) {
 			return false;
 		}
@@ -90,9 +95,11 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 
 	@Override
 	public UserModel getUserById(String id, RealmModel realm) {
+		LOG.info("Getting user by id {}...", id);
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USER_BY_ID);
-			st.setString(1, id);
+			String mysqlId = id.substring(id.lastIndexOf(':') + 1);
+			st.setString(1, mysqlId);
 			st.execute();
 			ResultSet rs = st.getResultSet();
 			if (rs.next()) {
@@ -107,6 +114,7 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 
 	@Override
 	public UserModel getUserByUsername(String username, RealmModel realm) {
+		LOG.info("Getting user by username {}...", username);
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USER_BY_USERNAME);
 			st.setString(1, username);
@@ -125,15 +133,17 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 	private UserModel mapUser(RealmModel realm, ResultSet rs) throws SQLException {
 		String[] firstlastname = rs.getString("user_name").split(" ");
 
-		return CustomUser.customUser(ksession, realm, model, rs.getString("id_user"))
+		return FoodbankMysqlUser.customUser(ksession, realm, model, rs.getString("id_user"))
 				.withEmail(rs.getString("email"))
 				.withFirstName(firstlastname[1])
 				.withLastName(firstlastname[0])
+				.withGroups(rs.getString("rights"))
 				.build();
 	}
 
 	@Override
 	public UserModel getUserByEmail(String email, RealmModel realm) {
+		LOG.info("Getting user by email {}...", email);
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USER_BY_EMAIL);
 			st.setString(1, email);
@@ -151,6 +161,7 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 
 	@Override
 	public int getUsersCount(RealmModel realmModel) {
+		LOG.info("Getting user count}...");
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USER_COUNT);
 			st.execute();
@@ -167,6 +178,7 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 
 	@Override
 	public List<UserModel> getUsers(RealmModel realm) {
+		LOG.info("Getting all users...");
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USERS);
 
@@ -183,12 +195,14 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 			return result;
 
 		} catch (SQLException ex) {
+			LOG.error("Database error", ex);
 			throw new RuntimeException("Database error: " + ex.getMessage(), ex);
 		}
 	}
 
 	@Override
 	public List<UserModel> getUsers(RealmModel realm, int firstResult, int maxResults) {
+		LOG.info("Getting users page...");
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USERS_PAGE);
 			st.setInt(1, firstResult);
@@ -212,6 +226,7 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 
 	@Override
 	public List<UserModel> searchForUser(String search, RealmModel realm) {
+		LOG.info("Searching user '{}'...", search);
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USERS_LOOKUP);
 			String searchLike = "%" + search + "%";
@@ -231,12 +246,14 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 			return result;
 
 		} catch (SQLException ex) {
+			LOG.error("Database error: ", ex);
 			throw new RuntimeException("Database error: " + ex.getMessage(), ex);
 		}
 	}
 
 	@Override
 	public List<UserModel> searchForUser(String search, RealmModel realm, int firstResult, int maxResults) {
+		LOG.info("Searching user '{}'...", search);
 		try (Connection c = DbUtil.getConnection(this.model)) {
 			PreparedStatement st = c.prepareStatement(USERS_LOOKUP_PAGE);
 			String searchLike = "%" + search + "%";
@@ -258,32 +275,80 @@ public class KeycloakFoodBankUserProvider implements UserStorageProvider, UserLo
 			return result;
 
 		} catch (SQLException ex) {
+			LOG.error("Database error", ex);
 			throw new RuntimeException("Database error: " + ex.getMessage(), ex);
 		}
 	}
 
 	@Override
 	public List<UserModel> searchForUser(Map<String, String> map, RealmModel realmModel) {
+		LOG.info("Search for user {}...", map);
 		return null;
 	}
 
 	@Override
-	public List<UserModel> searchForUser(Map<String, String> map, RealmModel realmModel, int i, int i1) {
+	public List<UserModel> searchForUser(Map<String, String> map, RealmModel realmModel, int firstResult, int maxResults) {
+		LOG.info("paginated Search for user {}...", map);
 		return null;
 	}
 
 	@Override
-	public List<UserModel> getGroupMembers(RealmModel realmModel, GroupModel groupModel) {
-		return null;
+	public List<UserModel> getGroupMembers(RealmModel realm, GroupModel groupModel) {
+		LOG.info("Get group members {}...", groupModel.getName());
+		String groupNameLike = "%" + groupModel.getName() + "%";
+		try (Connection c = DbUtil.getConnection(this.model)) {
+			PreparedStatement st = c.prepareStatement(USERS_BY_RIGHTS);
+
+			st.setString(1, groupNameLike);
+			st.execute();
+			ResultSet rs = st.getResultSet();
+
+			List<UserModel> result = new ArrayList<>();
+
+			while (rs.next()) {
+				UserModel userModel = mapUser(realm, rs);
+				result.add(userModel);
+			}
+
+			return result;
+
+		} catch (SQLException ex) {
+			LOG.error("Database error", ex);
+			throw new RuntimeException("Database error: " + ex.getMessage(), ex);
+		}
 	}
 
 	@Override
-	public List<UserModel> getGroupMembers(RealmModel realmModel, GroupModel groupModel, int i, int i1) {
-		return null;
+	public List<UserModel> getGroupMembers(RealmModel realm, GroupModel groupModel, int firstResult, int maxResults) {
+		LOG.info("Get group members {}...", groupModel.getName());
+		String groupNameLike = "%" + groupModel.getName() + "%";
+		try (Connection c = DbUtil.getConnection(this.model)) {
+			PreparedStatement st = c.prepareStatement(USERS_BY_RIGHTS_PAGE);
+
+			st.setString(1, groupNameLike);
+			st.setInt(2, firstResult);
+			st.setInt(3, maxResults);
+			st.execute();
+			ResultSet rs = st.getResultSet();
+
+			List<UserModel> result = new ArrayList<>();
+
+			while (rs.next()) {
+				UserModel userModel = mapUser(realm, rs);
+				result.add(userModel);
+			}
+
+			return result;
+
+		} catch (SQLException ex) {
+			LOG.error("Database error", ex);
+			throw new RuntimeException("Database error: " + ex.getMessage(), ex);
+		}
 	}
 
 	@Override
-	public List<UserModel> searchForUserByUserAttribute(String s, String s1, RealmModel realmModel) {
+	public List<UserModel> searchForUserByUserAttribute(String attribute, String value, RealmModel realmModel) {
+		LOG.info("Search by attribute {} with value {}...", attribute, value);
 		return null;
 	}
 }
