309396d6f8154c299c8adeaf0822676ac004523c
Alain Vandermeersch <alain.vandermeersch@gmail.com>
2023-07-30 16:49:40 +0200

    refuse deletion of organisation if there are movements for it 2

--- foodbank-rest-api/src/main/java/foodbank/it/service/impl/OrganisationServiceImpl.java
+++ foodbank-rest-api/src/main/java/foodbank/it/service/impl/OrganisationServiceImpl.java
@@ -71,9 +71,9 @@ public class OrganisationServiceImpl implements IOrganisationService{
 			throw new Exception(errorMsg);
 		}
 		CriteriaQuery<Long> totalMovementCriteriaQuery = criteriaBuilder.createQuery(Long.class);
-		Root<Movement> movement = totalCriteriaQuery.from(Movement.class);
+		Root<Movement> movement = totalMovementCriteriaQuery.from(Movement.class);
 		List<Predicate> movementPredicates = new ArrayList<>();
-		Predicate lienDisMovementPredicate = criteriaBuilder.equal(movement.get("idAsso"), String.valueOf(idDis));
+		Predicate lienDisMovementPredicate = criteriaBuilder.equal(criteriaBuilder.trim(movement.get("idAsso")), String.valueOf(idDis));
 		movementPredicates.add(lienDisMovementPredicate);
 		totalMovementCriteriaQuery.where(movementPredicates.stream().toArray(Predicate[]::new));
 		totalMovementCriteriaQuery.select(criteriaBuilder.count(movement));
