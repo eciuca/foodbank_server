46cfed720759ed68044e2dc3708137c3f34e2186
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2025-12-23 17:21:53 +0200

    refactor(keycloak): Enable HTTPS configuration in docker-compose and optimize realm import

--- foodbank-keycloak-user-provider/docker/entrypoint.sh
+++ foodbank-keycloak-user-provider/docker/entrypoint.sh
@@ -3,6 +3,6 @@ sed -i 's@###REDIRECT_URIS###@'"$REDIRECT_URIS"'@' /import/realm/*.json && \
     sed -i 's@###REDIRECT_URIS_ISIS###@'"$REDIRECT_URIS_ISIS"'@' /import/realm/*.json && \
     sed -i 's@###AUTH_SERVER_BASE_URL###@'"$AUTH_SERVER_BASE_URL"'@' /import/realm/*.json
 
-/opt/keycloak/bin/kc.sh --verbose import --dir=/import/realm
+/opt/keycloak/bin/kc.sh --verbose --optimized import --dir=/import/realm
 
 $@
