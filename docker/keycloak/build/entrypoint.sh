feda505bd588e827959a3042ece830eddf3e8624
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2025-12-23 12:56:11 +0200

    refactor(keycloak): Remove old keycloak build files

---
+++ docker/keycloak/build/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sed -i 's@###REDIRECT_URIS###@'"$REDIRECT_URIS"'@' /import/realm/*.json && \
+    sed -i 's@###REDIRECT_URIS_ISIS###@'"$REDIRECT_URIS_ISIS"'@' /import/realm/*.json && \
+    sed -i 's@###AUTH_SERVER_BASE_URL###@'"$AUTH_SERVER_BASE_URL"'@' /import/realm/*.json
+
+/opt/keycloak/bin/kc.sh --verbose import --dir=/import/realm
+
+$@
